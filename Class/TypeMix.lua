4031448d856a9bf6eabd65f818ffa0e838b04891
Curio <116847106+CuriousCurio@users.noreply.github.com>
2026-09-24 16:43:25 -0600

    Update TypeMix.lua

--- Class/TypeMix.lua
+++ Class/TypeMix.lua
@@ -26,7 +26,7 @@ local CONVERT = {
 	["userdata"] = function(v) return typeof(v) end,
 	["table"] = function(v) if (DECODE[v]) then return DECODE[v]; end end
 }
-local function EncodeType(v) -- encodes the type into a  table, function, or string
+local function EncodeType(v) -- encodes the type into a  string or table, function
 	local fx = CONVERT[type(v)]; -- checks if the type is in DECODE
 	if (fx) then return fx(v) or v; end -- if there is a function in DECODE it will returnn the type
 	return v;
